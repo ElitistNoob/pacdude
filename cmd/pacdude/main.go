cdf750f61ca326eba6d6892073aab617f0cfac40
Kevin Hernot <kevinhernot@Kevins-MacBook-Pro.local>
2026-02-16 20:14:13 +0700

    feat: add debug logger

--- cmd/pacdude/main.go
+++ cmd/pacdude/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	tui "github.com/ElitistNoob/pacdude/internal/tui"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	_, err := os.Stat("./debug.log")
+	if err != nil {
+		os.Create("debug.log")
+	}
+	os.Truncate("debug.log", 0)
+
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		fmt.Printf("Couldn't open log files: %s\n", err)
+	}
+	defer f.Close()
+
 	tui.Run()
 }
